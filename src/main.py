a4238416e16331ada27ffefd17a9bae301c87aa5
lucky-389 <girvarpatidar8955@gmail.com>
2024-04-19 15:26:45 +0530

    level7

--- src/main.py
+++ src/main.py
@@ -69,7 +69,7 @@ def start_level(screen, level_number):
     return level_number
 
 def main():
-    levels_unlocked = 1
+    levels_unlocked = 7
     total_levels = 10
     pygame.init()
     screen_width = 1080
